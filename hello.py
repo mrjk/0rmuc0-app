66dec21b78ef0eba5749d62d739158a5267abd75
rcordier <robin.cordier@stm.info>
2017-01-15 19:41:50 -0500

    Create the website pages

--- hello.py
+++ hello.py
@@ -2,5 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def page_main():
+    return 'The form should appear here!'
+
+@app.route('/list')
+def page_list():
+    return 'All records should be listed here!'
+
+
+@app.route('/list/<string:username>')
+def page_list_user(username):
+    return 'The record made by the user %s should appear here!' % username
