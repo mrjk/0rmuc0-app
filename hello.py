7e09b96936db17854288897da4703726b8793f22
rcordier <robin.cordier@stm.info>
2017-01-15 19:49:16 -0500

    All pages has been created, http methods implemented

--- hello.py
+++ hello.py
@@ -1,15 +1,20 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def page_main():
-    return 'The form should appear here!'
+    if request.method == 'POST':
+      return 'You have successfully posted your data!'
+    else:
+      return 'The form should appear here!'
 
-@app.route('/list')
+@app.route('/list/')
 def page_list():
     return 'All records should be listed here!'
 
 
-@app.route('/list/<string:username>')
+@app.route('/list/<string:username>/')
 def page_list_user(username):
     return 'The record made by the user %s should appear here!' % username
